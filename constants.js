40cb11d3fd0b0ccda8639a05b95f7b19a0e44fe3
Nishant Sharma <64049931+nishantsharma1115@users.noreply.github.com>
2022-01-12 00:17:55 +0530

    Filtering out the planet which is habitable...

---
+++ constants.js
@@ -0,0 +1,19 @@
+const FILE_PATH = 'planet_data.csv';
+const CONFIRMED_PLANET = 'CONFIRMED';
+const KOI_DISPOSITION = 'koi_disposition';
+const KOI_INSOL = 'koi_insol';
+const LOWEST_KOI_INSOL = 0.36;
+const HIGHEST_KOI_INSOL = 1.11;
+const KOI_PRAD = 'koi_prad';
+const TIMES_EARTH_RADII = 1.6;
+
+module.exports = {
+    FILE_PATH,
+    CONFIRMED_PLANET,
+    KOI_DISPOSITION,
+    KOI_INSOL,
+    LOWEST_KOI_INSOL,
+    HIGHEST_KOI_INSOL,
+    KOI_PRAD,
+    TIMES_EARTH_RADII
+}
--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const fs = require('fs');
 const parse = require('csv-parse');
+const constants = require('./constants.js');
 
-const results = [];
+const isHabitablePlanets = [];
 
-fs.createReadStream('planet_data.csv')
+// These are the factors that Kapler found in order to determine
+// whether a planet is habitable or not.
+function isHabitablePlanet(planet) {
+    return planet[constants.KOI_DISPOSITION] === constants.CONFIRMED_PLANET
+        && planet[constants.KOI_INSOL] > constants.LOWEST_KOI_INSOL
+        && planet[constants.KOI_INSOL] < constants.HIGHEST_KOI_INSOL
+        && planet[constants.KOI_PRAD] < constants.TIMES_EARTH_RADII;
+}
+
+fs.createReadStream(constants.FILE_PATH)
     .pipe(parse.parse({
         comment: '#',
         columns: true
     }))
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitablePlanet(data)) {
+            isHabitablePlanets.push(data);
+        }
     })
     .on('error', (err) => {
         console.error(err);
     })
     .on('end', () => {
-        console.log(results);
+        console.log(isHabitablePlanets);
         console.log('Done!!');
     });
 
